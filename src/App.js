9eeb26d51ea903215aa27a73f51fff6b0a3ac49d
k <handykameron@live.com>
2025-06-09 10:54:00 -0800

    adding table and updating navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import { useState } from 'react';
 import './App.css';
+import ToDoList from './Component/ToDoList';
+import Header from './Component/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [chores , setChores] = useState ([
+    
+    {id : 1 , MostImportant: "Take out the trash", Time:"When you want it to be completed"},
+    {id : 2, SemiImportant: "Wash the dishes" , Time:"When you want it to be completed"},
+    {id : 3, LeastImportant: "Put up the dishes", Time:"When you want it to be completed"}
+
+  ]) ;
+
   return (
-    <h1>To Do List</h1>
+    <>
+    < Header/>
+    <ToDoList chores={chores} setChores={setChores} />
+    
+    </>
 
   );
 }
